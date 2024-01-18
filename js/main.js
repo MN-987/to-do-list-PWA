092cc737691bca94622094d341de8be404dc2d57
Mostafa Nasser <mostafanasserm98@gmail.com>
2024-01-18 16:54:00 +0200

    initialized database and added first task

---
+++ js/main.js
@@ -0,0 +1,53 @@
+
+const openDb=async function(){
+    console.log(`entered`)
+    const dbPromise = await idb.openDB('Tasks',1)
+ console.log(dbPromise)
+}
+// openDb();
+
+
+ const createStoreInDB =async function (){
+    const dbPromise = await idb.openDB('Tasks', 1, {
+      upgrade (db) {
+    
+        if (!db.objectStoreNames.contains('user_task')) {
+        
+            const userTaskObjectStore=db.createObjectStore('user_task',{keyPath:'task_title'});
+
+            userTaskObjectStore.createIndex('task_title', 'task_title', { unique: false });
+          }      
+
+          else{
+            console.log(`else enterd`)
+          }
+    }
+    });
+  }
+  
+
+  const addTask =async function  () {
+    const db = await idb.openDB('Task', 1, {
+      upgrade (db) {
+        if (!db.objectStoreNames.contains('user_task')) {
+            const userTaskObjectStore=db.createObjectStore('user_task',{keyPath:'task_title'});
+
+            userTaskObjectStore.createIndex('task_title', 'task_title', { unique: false });
+        }
+      }
+    });
+    
+    const tx = db.transaction('user_task', 'readwrite');
+  
+    await Promise.all([
+      tx.store.add({
+        task_title: 'task 2',
+        created: new Date().getTime(),
+      }),
+      tx.done
+    ]);
+  }
+
+
+//   createStoreInDB();
+  addTask();
